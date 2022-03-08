241e593a142f27be15f2921372bb6b310fe5c2a6
HarikcUW <92060455+HarikcUW@users.noreply.github.com>
2022-03-08 13:19:04 -0800

    Update dataGenerator.py

--- myfaker/code/dataGenerator.py
+++ myfaker/code/dataGenerator.py
@@ -6,8 +6,8 @@ from .metricsProcessor import MetricsProcessor
 from .dataframeProcessor import DataframeProcessor
 
 class DataGenerator:
-  
-  
+
+
     def __init__(self, n, configList) -> None:
         self.__config = configList
         self.__n = MAX_SAMPLE_SIZE if n < 1 or n > MAX_SAMPLE_SIZE else n
@@ -29,7 +29,7 @@ class DataGenerator:
                     re_data = reObj.getDataFromRE()
                 elif(cfg['sourceType'] == 'Metrics'):
                     metricsConfig = cfg['values']
-                    metricsObj = MetricsProcessor(self.__n,metricsObj)
+                    metricsObj = MetricsProcessor(self.__n,metricsConfig)
                     metrics_data = metricsObj.processMetrics()
                 else:
                     raise Exception("Invalid sourceType value in cinfiguration: " + cfg['sourceType'])
@@ -37,8 +37,8 @@ class DataGenerator:
             df_data = df_data.reset_index(drop=True)
             re_data = re_data.reset_index(drop=True)
             metrics_data = metrics_data.reset_index(drop=True)
-        
-            final_data = pd.concat([df_data, re_data, metrics_data], axis=1)           
+
+            final_data = pd.concat([df_data, re_data, metrics_data], axis=1)
             return final_data
 
             raise Exception("Error in generating data")
