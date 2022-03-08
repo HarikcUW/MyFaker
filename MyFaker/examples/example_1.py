21af337f66214868cb02a0bebfabd7eefbfc93a5
HarikcUW <92060455+HarikcUW@users.noreply.github.com>
2022-03-07 16:42:09 -0800

    Update example_1.py

--- MyFaker/examples/example_1.py
+++ MyFaker/examples/example_1.py
@@ -1,3 +1,9 @@
+# Generate fake data with MyFaker library using regular expressions
+```
+This exampe will return random text for FirstName and Email as defined in regular expression 
+For Metrics, it will return randon value between start and end values. 
+```
+
 from MyFaker.code.helper import DataGenerator
 configList = [{'sourceType':'RegularExpression', 'values':[{'name':'Featurs'
                 ,'columns':[{'colName':'FirstName', 'prefix':'FirstName','sufix':'','regExpression':'[0-9]{2}'} 
@@ -7,6 +13,6 @@ configList = [{'sourceType':'RegularExpression', 'values':[{'name':'Featurs'
                              ,{'colName':'SalesAmount', 'dataType':'float', 'startValue':'10', 'endValue':'40'}]}]}
              ]
 
-dg = DataGenerator(10, configList)
-dfr = dg.processInput()
-print(dfr)
+fakeData = DataGenerator(10, configList)
+df_fakeData = fakeData.processInput()
+print(df_fakeData)
