7fa874a12a32918baf4812d1d0558bc20f0c52c4
HarikcUW <92060455+HarikcUW@users.noreply.github.com>
2022-03-07 17:24:33 -0800

    Update helper.py

--- MyFaker/code/helper.py
+++ MyFaker/code/helper.py
@@ -1,9 +1,7 @@
 import pandas as pd
-#import numpy as np
 import random
 import rstr
 from .constants import *
-#import constants
 
 class DataGenerator:
     
@@ -16,13 +14,19 @@ class DataGenerator:
 
     def isUpper(self, txt):
         return txt.isupper()
-
+    
+    # Get n random samples from dataframe
     def getDfElements(self, df, columnList):
         return df.sample(self.__n, replace=REPEAT_SAMPLE)[df.columns[df.columns.isin(columnList)]]
-
+    
+    # Rename data frmae columns using dictionary
     def renameDfColumn(self, df, columnRenameDict):
         return df.rename(columns=columnRenameDict)
 
+    '''
+    Read column details from configuration and get random samples from the dataframe
+    If rename columns specified create a dictionary and rename all columns once 
+    '''
     def getRandomDataFromDF(self, df, columns):
         if not columns:
             print("Dict is empty")
@@ -43,7 +47,11 @@ class DataGenerator:
 
         df_t = self.renameDfColumn(df_t, renameColumnsDict)
         return df_t
-
+    
+    '''
+    For each dataframe listed in configuration get random sample data
+    combine data from all data frames and generate single output
+    '''
     def getDataFromDF(self, configValue):
         rdf = pd.DataFrame()
         for params in configValue:
@@ -53,10 +61,10 @@ class DataGenerator:
             columns = params['columns']
             df_t = self.getRandomDataFromDF(df, columns)
             df_t = df_t.reset_index(drop=True)
-
             rdf = pd.concat([rdf, df_t], axis=1)
         return rdf
 
+    # Generate random value as defined in regula expression using rstr package
     def getRegExData(self,columnInfo):
         data = []
         columnName = columnInfo['colName']
@@ -67,9 +75,12 @@ class DataGenerator:
         for i in range(self.__n):
             data_regEx = prefix + rstr.xeger(regExp)
             data.append(data_regEx)
-
         return pd.DataFrame(data, columns=[columnName])
 
+    '''
+    For each column listed in RegulaExpression configuration get random sample data
+    combine data from all data frames and generate single output
+    '''
     def getDataFromRE(self, expressions):
         rdf = pd.DataFrame()
         for item in expressions:
@@ -78,9 +89,9 @@ class DataGenerator:
                 regExData = self.getRegExData(columnInfo)
                 regExData = regExData.reset_index(drop=True)
                 rdf = pd.concat([rdf, regExData], axis=1)
-            
         return rdf
-
+    
+    # Generate random int or float value between start and end value
     def getMetricData(self, columnInfo):
         columnName = columnInfo['colName']
         columnDataType = columnInfo['dataType']
@@ -107,9 +118,11 @@ class DataGenerator:
                 metricData = self.getMetricData(columnInfo)
                 metricData = metricData.reset_index(drop=True)
                 rdf = pd.concat([rdf, metricData], axis = 1)
-            
         return rdf 
-
+    ```
+    Process input configuration and return dataframe with define columns
+    expected values for sourceType item :  dataframe, RegularExpression, Metrics
+    ```
     def processInput(self):
         df_data = pd.DataFrame()
         re_data = pd.DataFrame()
@@ -122,13 +135,15 @@ class DataGenerator:
                 if(cfg['sourceType'] == 'dataframe'):
                     vals = cfg['values']
                     df_data = self.getDataFromDF(vals)
-                if(cfg['sourceType'] == 'RegularExpression'):
+                elif(cfg['sourceType'] == 'RegularExpression'):
                     vals = cfg['values']
                     re_data = self.getDataFromRE(vals)
-                if(cfg['sourceType'] == 'Metrics'):
+                elif(cfg['sourceType'] == 'Metrics'):
                     vals = cfg['values']
                     m_data = self.getMetricsData(vals)
-                    
+                else:
+                    raise Exception("Invalid sourcetype value in Config:" + cfg['sourceType'])
+                
             df_data = df_data.reset_index(drop=True)
             re_data = re_data.reset_index(drop=True)
             m_data = m_data.reset_index(drop=True)
