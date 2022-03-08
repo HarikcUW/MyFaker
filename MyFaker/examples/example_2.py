30ae4f41e53b6ce317de4dabe3a6c26fdbf0bd76
HarikcUW <92060455+HarikcUW@users.noreply.github.com>
2022-03-07 17:41:49 -0800

    Update example_2.py

--- MyFaker/examples/example_2.py
+++ MyFaker/examples/example_2.py
@@ -1,7 +1,7 @@
-```
+'''
 Generate fake data with MyFaker library using list and regular expressions
 For Metrics, it will return randon value between start and end values. 
-```
+'''
 import pandas as pd 
 from MyFaker.code.helper import DataGenerator
 
